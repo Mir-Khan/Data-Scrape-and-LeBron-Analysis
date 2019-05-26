defa8f6bcaf4dfc8e7d2c58c95f8645054019580
Mir-Khan <mirkhan1497@gmail.com>
2019-05-25 21:02:31 -0400

    Per-Game Update
    This is an update to the code with Per-Game stats, which were very similar in structure to per-36 minutes stats. Also, the percentage column names were simplified in order to access them easier in the future, as the data is the same across all pages.

--- scrape_Data.py
+++ scrape_Data.py
@@ -133,25 +133,25 @@ for page in per_36_pages:
                 '[data-stat=fg_per_mp]').text.strip())
             d['FGA_Per_36'] = float(row.select_one(
                 '[data-stat=fga_per_mp]').text.strip())
-            d['FG_PCT_Per_36'] = float(row.select_one(
+            d['FG_PCT'] = float(row.select_one(
                 '[data-stat=fg_pct]').text.strip())
             d['3PM_Per_36'] = float(row.select_one(
                 '[data-stat=fg3_per_mp]').text.strip())
             d['3PA_Per_36'] = float(row.select_one(
                 '[data-stat=fg3a_per_mp]').text.strip())
-            d['3P_PCT_Per_36'] = float(row.select_one(
+            d['3P_PCT'] = float(row.select_one(
                 '[data-stat=fg3_pct]').text.strip())
             d['2PM_Per_36'] = float(row.select_one(
                 '[data-stat=fg2_per_mp]').text.strip())
             d['2PA_Per_36'] = float(row.select_one(
                 '[data-stat=fg2a_per_mp]').text.strip())
-            d['2P_PCT_Per_36'] = float(row.select_one(
+            d['2P_PCT'] = float(row.select_one(
                 '[data-stat=fg2_pct]').text.strip())
             d['FTM_Per_36'] = float(row.select_one(
                 '[data-stat=ft_per_mp]').text.strip())
             d['FTA_Per_36'] = float(row.select_one(
                 '[data-stat=fta_per_mp]').text.strip())
-            d['FT_PCT_Per_36'] = float(row.select_one(
+            d['FT_PCT'] = float(row.select_one(
                 '[data-stat=ft_pct]').text.strip())
             d['DRB_Per_36'] = float(row.select_one(
                 '[data-stat=drb_per_mp]').text.strip())
@@ -175,7 +175,96 @@ for page in per_36_pages:
             data.append(d)
 
     year += 1
-    sleep(5)
+    sleep(3)
 
 with open('per_36.json', 'w') as f:
+    json.dump(data, f)
+
+#####################Multiple per Game Pages####################################
+# Per 36 Minutes stats from 2010-2018
+per_game = [
+    r'https://www.basketball-reference.com/leagues/NBA_2010_per_game.html',
+    r'https://www.basketball-reference.com/leagues/NBA_2011_per_game.html',
+    r'https://www.basketball-reference.com/leagues/NBA_2012_per_game.html',
+    r'https://www.basketball-reference.com/leagues/NBA_2013_per_game.html',
+    r'https://www.basketball-reference.com/leagues/NBA_2014_per_game.html',
+    r'https://www.basketball-reference.com/leagues/NBA_2015_per_game.html',
+    r'https://www.basketball-reference.com/leagues/NBA_2016_per_game.html',
+    r'https://www.basketball-reference.com/leagues/NBA_2017_per_game.html',
+    r'https://www.basketball-reference.com/leagues/NBA_2018_per_game.html'
+]
+
+data = []
+year = 2010  # This can be changed to whatever the starting year is
+
+for page in per_game:
+    r = requests.get(page)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    rows = soup.select('tbody tr')
+
+    for row in rows:
+        d = dict()
+
+        if row.select_one('[data-stat=g]').text.strip() != 'G' and row.select_one('[data-stat=fg3_pct]').text.strip() != "" and row.select_one('[data-stat=ft_pct]').text.strip() != "" and row.select_one('[data-stat=fg2_pct]').text.strip() != "":
+            d['Year'] = year
+            d['Name'] = row.select_one('[data-stat=player]').text.strip()
+            d['Position'] = row.select_one('[data-stat=pos]').text.strip()
+            d['Team'] = row.select_one('[data-stat=team_id]').text.strip()
+            d['Games_Played'] = float(
+                row.select_one('[data-stat=g]').text.strip())
+            d['Games_Started'] = float(
+                row.select_one('[data-stat=gs]').text.strip())
+            d['Minutes_Played_Per_Game'] = float(
+                row.select_one('[data-stat=mp_per_g]').text.strip())
+            d['FGM_Per_Game'] = float(row.select_one(
+                '[data-stat=fg_per_g]').text.strip())
+            d['FGA_Per_Game'] = float(row.select_one(
+                '[data-stat=fga_per_g]').text.strip())
+            d['FG_PCT'] = float(row.select_one(
+                '[data-stat=fg_pct]').text.strip())
+            d['3PM_Per_Game'] = float(row.select_one(
+                '[data-stat=fg3_per_g]').text.strip())
+            d['3PA_Per_Game'] = float(row.select_one(
+                '[data-stat=fg3a_per_g]').text.strip())
+            d['3P_PCT'] = float(row.select_one(
+                '[data-stat=fg3_pct]').text.strip())
+            d['2PM_Per_Game'] = float(row.select_one(
+                '[data-stat=fg2_per_g]').text.strip())
+            d['2PA_Per_Game'] = float(row.select_one(
+                '[data-stat=fg2a_per_g]').text.strip())
+            d['2P_PCT'] = float(row.select_one(
+                '[data-stat=fg2_pct]').text.strip())
+            d['EFG_PCT'] = float(row.select_one(
+                '[data-stat=efg_pct]').text.strip())
+            d['FTM_Per_Game'] = float(row.select_one(
+                '[data-stat=ft_per_g]').text.strip())
+            d['FTA_Per_Game'] = float(row.select_one(
+                '[data-stat=fta_per_g]').text.strip())
+            d['FT_PCT'] = float(row.select_one(
+                '[data-stat=ft_pct]').text.strip())
+            d['DRB_Per_Game'] = float(row.select_one(
+                '[data-stat=drb_per_g]').text.strip())
+            d['ORB_Per_Game'] = float(row.select_one(
+                '[data-stat=orb_per_g]').text.strip())
+            d['TRB_Per_Game'] = float(row.select_one(
+                '[data-stat=trb_per_g]').text.strip())
+            d['AST_Per_Game'] = float(row.select_one(
+                '[data-stat=ast_per_g]').text.strip())
+            d['STL_Per_Game'] = float(row.select_one(
+                '[data-stat=stl_per_g]').text.strip())
+            d['BLK_Per_Game'] = float(row.select_one(
+                '[data-stat=blk_per_g]').text.strip())
+            d['TOV_Per_Game'] = float(row.select_one(
+                '[data-stat=tov_per_g]').text.strip())
+            d['Fouls_Per_Game'] = float(row.select_one(
+                '[data-stat=pf_per_g]').text.strip())
+            d['Points_Per_Game'] = float(row.select_one(
+                '[data-stat=pts_per_g]').text.strip())
+
+            data.append(d)
+
+    year += 1
+    sleep(3)
+
+with open('per_game.json', 'w') as f:
     json.dump(data, f)
